e07901a359ede5258d20e4359adaf8f5fe1eae5d
toondebooser <toondebooser@gmail.com>
2023-05-09 17:19:48 +0200

    Set up basic signup form with basic styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,21 @@ import Home from "./Home";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import Dashboard from "./Dashboard";
+import Signup from "./Signup";
 
 function App() {
   return (
     <>
-      <Router>
-      <Routes>
-        <Route path="/Login" Component={LoginForm}/>
-        <Route exact path="/" Component={Home}/>
-        <Route path="/Dashboard" Component={Dashboard}/>
-      </Routes>
-      </Router>
+      <div className="content">
+        <Router>
+          <Routes>
+            <Route path="/Login" Component={LoginForm} />
+            <Route exact path="/" Component={Home} />
+            <Route path="/Dashboard" Component={Dashboard} />
+            <Route path="/Signup" Component={Signup} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useRef } from 'react'
+
+export default function Signup() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmationRef = useRef()
+  return (
+    <>
+    <div className="card">
+        <h2 className='formTitle'>Sign up</h2>
+        <form>
+            <span className="email">
+                <label  htmlFor="email">Email</label> <br />
+                <input required type="email" name='email' ref={emailRef} />
+            </span>
+
+            <span className="password">
+                <label htmlFor="password">Password</label><br />
+                <input type="password" name='password' ref={passwordRef} />
+            </span>
+
+            <span className="passwordConfirmation">
+                <label htmlFor="passwordConfirmation">Password Confirmation</label><br />
+                <input type="password" name='passwordConfirmation' ref={passwordConfirmationRef} />
+            </span>
+
+
+            <span className="submit"><input className='submitButton' type="submit" value="Sign up" /></span>
+        </form>
+    </div>
+    </>
+  )
+}
